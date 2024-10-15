2b808f73eeafba53c90b21a0e66d69f0efc740c8
srpmfp <sean@mothnflame.com>
2024-10-15 07:36:44 -0400

    updated type to types

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const bulbasaur = { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'] };
-const charmander = { name: 'Charmander', height: 0.6, type: ['fire'] };
-const squirtle = { name: 'Squirtle', height: 0.5, type: ['Water'] };
+const bulbasaur = { name: 'Bulbasaur', height: 0.7, types: ['Grass', 'Poison'] };
+const charmander = { name: 'Charmander', height: 0.6, types: ['fire'] };
+const squirtle = { name: 'Squirtle', height: 0.5, types: ['Water'] };
 const pokemonList = [bulbasaur, charmander, squirtle];
