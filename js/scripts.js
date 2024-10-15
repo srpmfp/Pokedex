2c6f0c4f851094e40c79ddf54a29eeeaa61ae99e
srpmfp <sean@mothnflame.com>
2024-10-15 07:32:19 -0400

    updated variables to be const since no changes will be made

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let bulbasaur = { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'] };
-let charmander = { name: 'Charmander', height: 0.6, type: ['fire'] };
-let squirtle = { name: 'Squirtle', height: 0.5, type: ['Water'] };
-pokemonList = [bulbasaur, charmander, squirtle];
+const bulbasaur = { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'] };
+const charmander = { name: 'Charmander', height: 0.6, type: ['fire'] };
+const squirtle = { name: 'Squirtle', height: 0.5, type: ['Water'] };
+const pokemonList = [bulbasaur, charmander, squirtle];
