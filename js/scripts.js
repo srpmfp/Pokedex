2153d7cd278fb403e765ca16d889b593fd2d677e
srpmfp <sean@mothnflame.com>
2024-10-14 21:47:02 -0400

    created 3 pokemon and their basic info

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-alert("Hello, it it me you're looking for?");
-
-let favfood = "Bean juice, AKA coffee";
-document.write(favfood);
+let bulbasaur = { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'] };
+let charmander = { name: 'Charmander', height: 0.6, type: ['fire'] };
+let squirtle = { name: 'Squirtle', height: 0.5, type: ['Water'] };
+pokemonList = [bulbasaur, charmander, squirtle];
