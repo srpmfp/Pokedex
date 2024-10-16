7daed1c9fbe53f44455d2c7bf8ddf9a371963e6d
srpmfp <sean@mothnflame.com>
2024-10-15 23:42:12 -0400

    removed stray var declaration w/ loop comments

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,16 @@ const pokemonList = [bulbasaur, charmander, squirtle];
 
 //listing out the name and height of the pokemon in the pokemonList 
 for (let i = 0; i < pokemonList.length; i++){
-    var x;
     
+    // heights greater than .5 less than .7
     if (pokemonList[i].height > 0.5 && pokemonList[i].height < 0.7) {
         document.write("<br>" + pokemonList[i].name + " (Height " + pokemonList[i].height + ")");
     }
+    // heights less than or equal to .5
     else if (pokemonList[i].height <= 0.5) {
         document.write("<br> " + pokemonList[i].name + " (Height " + pokemonList[i].height + "). ");
     }
+    // heights greater than 0.6
     else if (pokemonList[i].height > 0.6) {
         document.write(" " + pokemonList[i].name + " (Height " + pokemonList[i].height + ")-Wow, you are so big!!");
     }
