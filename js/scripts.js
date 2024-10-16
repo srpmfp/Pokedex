2f10b6c3b8b2b10696855f1f6ac0daf01fcb3858
srpmfp <sean@mothnflame.com>
2024-10-15 21:24:57 -0400

    loop through .length for ht > 0.6

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,20 @@ const bulbasaur = { name: 'Bulbasaur', height: 0.7, types: ['Grass', 'Poison'] }
 const charmander = { name: 'Charmander', height: 0.6, types: ['fire'] };
 const squirtle = { name: 'Squirtle', height: 0.5, types: ['Water'] };
 const pokemonList = [bulbasaur, charmander, squirtle];
+
+//listing out the name and height of the pokemon in the pokemonList 
+for (let i = 0; i < pokemonList.length; i++){
+    var x;
+    
+    if (pokemonList[i].height > 0.5 && pokemonList[i].height < 0.7) {
+        document.write("<br>" + pokemonList[i].name + " (Height " + pokemonList[i].height + ")");
+    }
+    else if (pokemonList[i].height <= 0.5) {
+        document.write("<br> " + pokemonList[i].name + " (Height " + pokemonList[i].height + "). ");
+    }
+    else if (pokemonList[i].height > 0.6) {
+        document.write(" " + pokemonList[i].name + " (Height " + pokemonList[i].height + ")-Wow, you are so big!!");
+    }
+
+}
+
