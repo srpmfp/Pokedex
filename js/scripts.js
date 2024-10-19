f1b8fb6b94a8da4e18de564a1e3e944e4ff563ab
srpmfp <sean@mothnflame.com>
2024-10-19 19:58:14 -0400

    List creation and event listening on pokebutton
    created console.log for event handler.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,75 @@
 
 
- let pokemonRepository = (function () {
-    
-     //list of manually uploaded pokemon
+let pokemonRepository = (function () {
+
+    //list of manually uploaded pokemon
     let pokemonList = [
-         { name: 'Bulbasaur', height: 0.7, types: ['Grass', 'Poison'] },
-         { name: 'Charmander', height: 0.6, types: ['fire'] },
-         { name: 'Squirtle', height: 0.5, types: ['Water'] }
-     ];  
-    
-     /////////////add new function/////////////// 
-     
-     function add(pokemon) {
-        pokemonList.push(pokemon);
-     }
-     
+        { name: 'Bulbasaur', height: 0.7, types: ['Grass', 'Poison'] },
+        { name: 'Charmander', height: 0.6, types: ['fire'] },
+        { name: 'Squirtle', height: 0.5, types: ['Water'] }
+    ];
+
+    /////////////List Out Pokemon/////////////// 
+
+    function addListItem(pokemon) {
+        // ul pokemon list selector
+        let selectedList = document.querySelector('.pokemonSelect');
+
+        // create new list element
+        let createLi = document.createElement('li');
+
+        //  create new button element
+        let createButton = document.createElement('button')
+
+
+        // individual list item selector
+        let pokeSelect = document.querySelector('.pokemon');
+
+        //adds button to lI
+        createButton.addEventListener('click', function (event) {
+            let target = event.target;
+            return showDetails(pokemon);
+        });
+
+        createButton.innerText = pokemon.name;
+        createButton.classList.add('pokeButton');
+        createLi.appendChild(createButton);
+
+
+
+        //adds li to ul
+        createLi.classList.add('pokemon');
+        selectedList.prepend(createLi);
+
+
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+
+
     /////////////return all///////////////
-    
-     function getAll() {
+
+    function getAll() {
         return pokemonList;
-     }
+    }
 
 
     ////////////functions call//////////////// 
-     return {
-         getAll: getAll,
-         add: add
-     }
-     
+    return {
+        getAll: getAll,
+        showDetails: showDetails,
+        addListItem: addListItem
+    }
+
 })()
 
-function listBig(pokemon) {
-  
-    // heights greater than .5 less than .7
-    if (pokemon.height > 0.5 && pokemon.height < 0.7) {
-        return document.write("<br>" + pokemon.name + " (Height " + pokemon.height + ")");
-    }
-    // heights less than or equal to .5
-    else if (pokemon.height <= 0.5) {
-        return document.write("<br>" + pokemon.name + " (Height " + pokemon.height + ")");
-    }
-    // heights greater than 0.6
-    else if (pokemon.height > 0.6) {
-        return document.write(" " + pokemon.name + " (Height " + pokemon.height + ")-Wow, you are so big!!");
-    }
+function listPokemon(pokemon) {
+    pokemonRepository.addListItem(pokemon);
 }
 
-pokemonRepository.getAll().forEach(listBig);
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
